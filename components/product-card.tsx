b2614978b34543feaf1ca3c6e7ae10b09b24bdaf
Peyman Bayat <p2436b@gmail.com>
2024-11-09 23:24:14 +0300

    Sample product card design

--- components/product-card.tsx
+++ components/product-card.tsx
@@ -1,7 +1,18 @@
-import React from 'react'
+import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from './ui/card';
 
 export default function ProductCard() {
 	return (
-		<div>ProductCard</div>
-	)
+		<Card>
+			<CardHeader>
+				<CardTitle>Card Title</CardTitle>
+				<CardDescription>Card Description</CardDescription>
+			</CardHeader>
+			<CardContent>
+				<p>Card Content</p>
+			</CardContent>
+			<CardFooter>
+				<p>Card Footer</p>
+			</CardFooter>
+		</Card>
+	);
 }
